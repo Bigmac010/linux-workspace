b39680b3b0cd2369051afe0b0e2fbeb4750feb4f
Bigmac010 <yok9904@gmail.com>
2026-07-21 14:42:07 +0000

    Add Evan LaTeX style

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ WORKSPACE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 mkdir -p "$HOME/.config/fish"
 mkdir -p "$HOME/.config/nvim"
+mkdir -p "$HOME/texmf/tex/latex"
 
 ln -sfn \
     "$WORKSPACE_DIR/dotfiles/.config/fish/config.fish" \
@@ -14,6 +15,11 @@ ln -sfn \
 ln -sfn \
     "$WORKSPACE_DIR/dotfiles/.config/nvim/init.lua" \
     "$HOME/.config/nvim/init.lua"
+
+ln -sfnT \
+    "$WORKSPACE_DIR/dotfiles/texmf/tex/latex/evan" \
+    "$HOME/texmf/tex/latex/evan"
+
 
 git config --global core.editor nvim
 git config --global init.defaultBranch main
