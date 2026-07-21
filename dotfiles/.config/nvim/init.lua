65e07e8fae334052a7e284fc521e4b954e3faf6b
Bigmac010 <yok9904@gmail.com>
2026-07-21 13:20:56 +0100

    Add NeoVim and VimTeX configuration
    Configures NeoVim editing preferences, installs the Lazy plugin manager and VimTeX, and uses latexmk for compiling LaTeX documents.

---
+++ dotfiles/.config/nvim/init.lua
@@ -0,0 +1,49 @@
+vim.g.mapleader = " "
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.mouse = "a"
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.smartindent = true
+vim.opt.wrap = false
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+local lazy_path = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local uv = vim.uv or vim.loop
+
+if not uv.fs_stat(lazy_path) then
+    local result = vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable",
+        lazy_path
+    })
+
+    if vim.v.shell_error ~= 0 then
+        error("Failed to install lazy.nvim:\n" .. result)
+    end
+end
+
+vim.opt.rtp:prepend(lazy_path)
+
+require("lazy").setup({
+    {
+        "lervag/vimtex",
+        lazy = false,
+        init = function()
+            vim.g.vimtex_compiler_method = "latexmk"
+            vim.g.vimtex_quickfix_mode = 0
+
+            if vim.env.CODESPACES == "true" then
+                vim.g.vimtex_view_enabled = 0
+            else
+                vim.g.vimtex_view_method = "zathura"
+            end
+        end
+    }
+})
